217bb0b1f4718cee3a39e4248017218e864eed0f
MeeetPandya <meetpandya.dev@gmail.com>
2025-06-27 18:06:02 +0530

    add: Get endpoint to  requst the readme for any project

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 import { GoogleGenAI } from "@google/genai";
+import express from "express";
 import { config } from "dotenv";
 
 config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const ai = new GoogleGenAI({
 apiKey: process.env.GEMINI_API_KEY,
 });
 
-async function main() {
-    console.log("Starting AI content generation...");
+async function generateReadme(repoUrl) {
+    console.log("Generating README for : ", repoUrl);
+
+    const PROMPT = `Given is a github repository link, generate a README.md file for it. ${repoUrl}`;
+
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Given is a github repository link, generate a README.md file for it. https://github.com/MeeetPandya/portfolio",
+        contents:[{
+            parts: [{ text: PROMPT}]
+        }]
     });
-    console.log(response.text);
+    
+    const text = response.text || "No content generated";
+    return text;
 }
 
-main()
+//GET Route to generate README
+app.get("/generate-readme", async (req,res) => {
+    const repoUrl = req.query.repo;
+
+    if (!repoUrl) {
+        return res.status(400).send("Repository URL is required");
+    }
+
+    try {
+        const result = await generateReadme(repoUrl);
+        res.type("text/plain").send(result);
+    } catch (error) {
+        console.error("Error generating README:", error);
+        res.status(500).send("Error generating README");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);   
+
+});
